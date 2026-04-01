f328f1eb5dcbeeeb7b1f850e11f7038e63595a62
Pietro Pasini <pietro.pasini4@studio.unibo.it>
2026-04-01 09:31:53 +0200

    a05b finished

--- src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
@@ -4,16 +4,20 @@ import it.unibo.pps.ex.Vector2D
 import it.unibo.pps.polyglot.a05b.Logics
 import it.unibo.pps.util.Sequences.Sequence
 import Sequence.*
+import  Math.abs
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
 
-  private val random = scala.util.Random(42)
-  private val centre: (Int, Int) = (random.nextInt(size - 2) + 1, random.nextInt(size - 2) + 1)
+  private val random = scala.util.Random()
+  private val cx = random.nextInt(size - 2) + 1
+  private val cy = random.nextInt(size - 2) + 1
+  private val centre = (cx, cy)
   private var epoch = 0
 
   override def tick(): Unit = epoch += 1
 
-  override def isOver: Boolean = Math.min(centre._1, centre._2) - epoch < 0 || Math.max(centre._1, centre._2) - epoch >= size
+  override def isOver: Boolean = !centre.toList.forall(d => epoch <= d && epoch < size - d)
 
-  override def hasElement(x: Int, y: Int): Boolean = 
+  override def hasElement(x: Int, y: Int): Boolean =
+    abs(x - cx) <= epoch && abs(y - cy) <= epoch && (x == cx || y == cy || abs(x - cx) == abs(y - cy))
